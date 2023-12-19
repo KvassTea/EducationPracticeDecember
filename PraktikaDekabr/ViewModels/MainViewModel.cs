14c8da34ef31484181dfbd94c797bc1db1e149c2
KvassTea <Kvass>
2023-12-19 12:54:55 +0800

    Добавлены остальные компоненты MVVM модели, позволяющие манипулировать данными с пользователями

---
+++ PraktikaDekabr/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using PraktikaDekabr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PraktikaDekabr.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Поля
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        //Метод
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Добро пожаловать {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Неправильное имя пользователя, не получается войти в систему";
+
+            }
+        }
+    }
+}
